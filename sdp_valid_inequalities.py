17ff068ece5ba4ef9fc135d3cc2c519af07e0ccd
Diego LG <dilagu@correo.ugr.es>
2026-06-22 10:10:05 +0200

    mas desigualdades

--- sdp_valid_inequalities.py
+++ sdp_valid_inequalities.py
@@ -30,6 +30,8 @@ def weber(demands, p, ineq):
     B = [M.variable(f"B{j}", [n,n], Domain.greaterThan(0)) for j in range(p)]
     C = [M.variable(f"C{j}", Domain.inPSDCone(n)) for j in range(p)]
 
+    Z = [M.variable(f"Z{j}", Domain.inPSDCone(3*n)) for j in range(p)]
+
     d = M.variable("d", [n,p], Domain.greaterThan(0))
     y = M.variable("y", [p,dim], Domain.unbounded())
 
@@ -40,13 +42,12 @@ def weber(demands, p, ineq):
     for j in range(p):
         M.constraint(trace(P[j]) == 1)
 
-        Zj = Expr.stack([
+        M.constraint(Z[j] == Expr.stack([
             [Expr.constTerm(Matrix.eye(n)), P[j], D[j]],
             [P[j], P[j], B[j].T],
             [D[j], B[j], C[j]],
-        ])
-        M.constraint(Zj, Domain.inPSDCone(3*n))
-        M.constraint(Zj >= 0)
+        ]))
+        M.constraint(Z[j] >= 0)
 
         for i in range(n):
             dd = Expr.vstack([d[i,j]] + [y[j,k] - demands[i][k] for k in range(dim)])
@@ -54,12 +55,9 @@ def weber(demands, p, ineq):
 
             for k in range(i+1):
                 M.constraint(D[j][i,k] == d[i,j] + d[k,j])
-        
+
         if ineq["I<P sdp bound"]:
             M.constraint(Matrix.eye(n) - sum(P), Domain.inPSDCone(n))
-
-        if ineq["distance square diameter bounds"]:
-            M.constraint(C[j] <= Matrix.dense(n,n,n*diam2*4))
         
         if ineq["diagonal bounds"]:
             for i in range(n):
@@ -72,15 +70,6 @@ def weber(demands, p, ineq):
                     M.constraint(P[j][i,k] <= P[j][k,k])
         
         if ineq["2x2 minors"]:
-            for i in range(n):
-                for k in range(i):
-                    M.constraint(Expr.stack([
-                        [P[j][i,i], P[j][i,k]],
-                        [P[j][k,i], P[j][k,k]]
-                    ]), Domain.inPSDCone())
-        
-        
-        if ineq["nxn minors"]:
             for i in range(n):
                 for k in range(i):
                     M.constraint(Expr.stack([
@@ -106,6 +95,32 @@ def weber(demands, p, ineq):
             for i in range(n):
                 for k in range(i):
                     M.constraint(D[j][i,k] >= sqrt(sum([(demands[i][r] - demands[k][r])**2 for r in range(dim)]))) # unweighted
+
+        if ineq["boros hammer"]:
+            for _ in range(10):
+                w = np.random.randint(-10, 10+1, n+1)
+                M.constraint(w[n]*(w[n]-1) + sum([w[i]*(w[i] + 2*w[n] - 1)*P[j][i,i] for i in range(n)]) + 2*sum([w[i]*w[k]*P[j][i,k] for k in range(i) for i in range(n)]) >= 0)
+
+        if ineq["distance projection linking cuts"]:
+            for i in range(n):
+                for k in range(i):
+                    deltaik = np.linalg.norm(np.array(demands[i]) - np.array(demands[k]))
+                    M.constraint(D[j][i,k] >= deltaik)
+                    M.constraint(B[j][i,k] >= deltaik * P[j][i,k])
+
+        if ineq["distance square diameter bounds"]:
+            M.constraint(C[j] <= Matrix.dense(n,n,n*diam2*4))
+            
+        if ineq["cluster symmetry break"]:
+            for i in range(p-1):
+                for j in range(i+1,p):
+                    M.constraint(P[j][i,i] == 0)
+        
+    
+    if ineq["rlt assignment consistency"]:
+        for i in range(n):
+            for k in range(n):
+                M.constraint(sum([P[j][i,i] + P[j][k,k] - 2 * P[j][i,k] for j in range(p)]) >= 0)
     
     M.solve()
 
@@ -113,6 +128,7 @@ def weber(demands, p, ineq):
     currentB = [np.reshape(B[j].level(), (n,n)) for j in range(p)]
     currentC = [np.reshape(C[j].level(), (n,n)) for j in range(p)]
     currentD = [np.reshape(D[j].level(), (n,n)) for j in range(p)]
+    currentZ = [np.reshape(Z[j].level(), (3*n,3*n)) for j in range(p)]
     currenty = y.level()
 
     score = 0
@@ -138,25 +154,28 @@ demands = [[0,0],[1,0],[0,1]]
 p = 2
 
 inequalities = {
-    "I<P sdp bound" : True,
-    "diagonal bounds" : True,
-    "off-diagonal bounds" : True,
-    "2x2 minors" : True,
-    "nxn minors" : True,
-    "triangle inequalities" : True,
-    "distance sym and nn" : True,
-    "pairwise distance lb" : True,
-    "eigencuts" : True,
-    "sparse eigencuts" : True,
-    "sparse dnn eigencuts" : True,
-    "boros hammer" : True,
-    "clique inequalities" : True,
-    "odd-cycle inequalities" : True,
-    "socrlt perspective cuts" : True,
-    "rlt assignment consistency" : True,
-    "distance projection linking cuts" : True,
-    "geometric conflict cuts" : True,
-    "distance square diameter bounds" : True
+    "I<P sdp bound" : False,
+    "diagonal bounds" : False,
+    "off-diagonal bounds" : False,
+    "2x2 minors" : False,
+    "triangle inequalities" : False,
+    "distance sym and nn" : False,
+    "pairwise distance lb" : False,
+    "eigencuts" : False,
+    "sparse eigencuts" : False,
+    "sparse dnn eigencuts" : False,
+    "boros hammer" : False,
+    "clique inequalities" : False,
+    "odd-cycle inequalities" : False,
+    "socrlt perspective cuts" : False,
+    "rlt assignment consistency" : False,
+    "distance projection linking cuts" : False,
+    "geometric conflict cuts" : False,
+    "distance square diameter bounds" : False,
+    "cluster symmetry break" : False
 }
+
+for k in inequalities.keys():
+    inequalities[k] = True
 
 weber(demands, p, inequalities)
