c7eb91dc46af21ac2ec0f68808aefde388992d48
Diego <dilagu@correo.ugr.es>
2026-06-30 12:51:22 +0200

    260630

--- sdp_valid_inequalities.py
+++ sdp_valid_inequalities.py
@@ -5,12 +5,13 @@ import numpy as np
 from math import sqrt
 import sys
 
-np.set_printoptions(precision=5, suppress=True)
-
 def trace(X):
     return Expr.dot(Matrix.eye(X.shape[0]),X)
 
-def weber(demands, p, ineq):
+def diag(X):
+    return Expr.vstack([X[i,i] for i in range(X.shape[0])])
+
+def weber(demands, p, ineq=None):
     n = len(demands)
     dim = len(demands[0])
 
@@ -22,8 +23,8 @@ def weber(demands, p, ineq):
                 diam2 = temp
     
     M = Model()
-    M.setSolverParam("log", 20)
-    M.setLogHandler(sys.stdout)
+    #M.setSolverParam("log", 20)
+    #M.setLogHandler(sys.stdout)
 
     P = [M.variable(f"P{j}", Domain.inPSDCone(n)) for j in range(p)]
     D = [M.variable(f"D{j}", [n,n], Domain.greaterThan(0)) for j in range(p)]
@@ -56,102 +57,104 @@ def weber(demands, p, ineq):
             for k in range(i+1):
                 M.constraint(D[j][i,k] == d[i,j] + d[k,j])
 
-        if ineq["I<P sdp bound"]:
-            M.constraint(Matrix.eye(n) - sum(P), Domain.inPSDCone(n))
-        
-        if ineq["diagonal bounds"]:
-            for i in range(n):
-                M.constraint(P[j][i,i] <= 1)
-        
-        if ineq["off-diagonal bounds"]:
-            for i in range(n):
-                for k in range(i):
-                    M.constraint(P[j][i,k] <= P[j][i,i])
-                    M.constraint(P[j][i,k] <= P[j][k,k])
-        
-        if ineq["2x2 minors"]:
-            for i in range(n):
-                for k in range(i):
-                    M.constraint(Expr.stack([
-                        [P[j][i,i], P[j][i,k]],
-                        [P[j][k,i], P[j][k,k]]
-                    ]), Domain.inPSDCone())
-        
-        if ineq["triangle inequalities"]:
-            for i in range(n):
-                for k in range(i):
-                    for l in range(i):
-                        M.constraint(P[j][i,k] >= P[j][i,l] + P[j][k,l] - P[j][l,l])
-                        M.constraint(P[j][i,l] >= P[j][i,k] + P[j][k,l] - P[j][k,k])
-                        M.constraint(P[j][k,l] >= P[j][i,k] + P[j][i,l] - P[j][i,i])
-                        
-                        M.constraint(P[j][i,k] + P[j][i,l] + P[j][k,l] >= P[j][i,i] + P[j][k,k] + P[j][l,l] - 1)
-        
-        if ineq["distance sym and nn"]:
-            M.constraint(D[j].T == D[j])
-            M.constraint(D[j] >= 0)
-        
-        if ineq["pairwise distance lb"]:
-            for i in range(n):
-                for k in range(i):
-                    M.constraint(D[j][i,k] >= sqrt(sum([(demands[i][r] - demands[k][r])**2 for r in range(dim)]))) # unweighted
-
-        if ineq["boros hammer"]:
-            for _ in range(10):
-                w = np.random.randint(-10, 10+1, n+1)
-                M.constraint(w[n]*(w[n]-1) + sum([w[i]*(w[i] + 2*w[n] - 1)*P[j][i,i] for i in range(n)]) + 2*sum([w[i]*w[k]*P[j][i,k] for k in range(i) for i in range(n)]) >= 0)
-
-        if ineq["distance projection linking cuts"]:
-            for i in range(n):
-                for k in range(i):
-                    deltaik = np.linalg.norm(np.array(demands[i]) - np.array(demands[k]))
-                    M.constraint(D[j][i,k] >= deltaik)
-                    M.constraint(B[j][i,k] >= deltaik * P[j][i,k])
-
-        if ineq["distance square diameter bounds"]:
-            M.constraint(C[j] <= Matrix.dense(n,n,n*diam2*4))
+    if ineq:
+        for j in range(p):
+            if ineq["I<P sdp bound"]:
+                M.constraint(Matrix.eye(n) - sum(P), Domain.inPSDCone(n))
             
-        if ineq["cluster symmetry break"]:
-            for i in range(p-1):
-                for j in range(i+1,p):
-                    M.constraint(P[j][i,i] == 0)
+            if ineq["diagonal bounds"]:
+                for i in range(n):
+                    M.constraint(P[j][i,i] <= 1)
+            
+            if ineq["off-diagonal bounds"]:
+                for i in range(n):
+                    for k in range(i):
+                        M.constraint(P[j][i,k] <= P[j][i,i])
+                        M.constraint(P[j][i,k] <= P[j][k,k])
+            
+            if ineq["2x2 minors"]:
+                for i in range(n):
+                    for k in range(i):
+                        M.constraint(Expr.stack([
+                            [P[j][i,i], P[j][i,k]],
+                            [P[j][k,i], P[j][k,k]]
+                        ]), Domain.inPSDCone())
+            
+            if ineq["triangle inequalities"]:
+                for i in range(n):
+                    for k in range(i):
+                        for l in range(i):
+                            M.constraint(P[j][i,k] >= P[j][i,l] + P[j][k,l] - P[j][l,l])
+                            M.constraint(P[j][i,l] >= P[j][i,k] + P[j][k,l] - P[j][k,k])
+                            M.constraint(P[j][k,l] >= P[j][i,k] + P[j][i,l] - P[j][i,i])
+                            
+                            M.constraint(P[j][i,k] + P[j][i,l] + P[j][k,l] >= P[j][i,i] + P[j][k,k] + P[j][l,l] - 1)
+            
+            if ineq["distance sym and nn"]:
+                M.constraint(D[j].T == D[j])
+                M.constraint(D[j] >= 0)
+            
+            if ineq["pairwise distance lb"]:
+                for i in range(n):
+                    for k in range(i):
+                        M.constraint(D[j][i,k] >= sqrt(sum([(demands[i][r] - demands[k][r])**2 for r in range(dim)]))) # unweighted
+    
+            if ineq["boros hammer"]:
+                for _ in range(10):
+                    w = np.random.randint(-10, 10+1, n+1)
+                    M.constraint(w[n]*(w[n]-1) + sum([w[i]*(w[i] + 2*w[n] - 1)*P[j][i,i] for i in range(n)]) + 2*sum([w[i]*w[k]*P[j][i,k] for k in range(i) for i in range(n)]) >= 0)
+    
+            if ineq["distance projection linking cuts"]:
+                for i in range(n):
+                    for k in range(i):
+                        deltaik = np.linalg.norm(np.array(demands[i]) - np.array(demands[k]))
+                        M.constraint(D[j][i,k] >= deltaik)
+                        M.constraint(B[j][i,k] >= deltaik * P[j][i,k])
+    
+            if ineq["distance square diameter bounds"]:
+                M.constraint(C[j] <= Matrix.dense(n,n,n*diam2*4))
+                
+            if ineq["cluster symmetry break"]:
+                for i in range(p-1):
+                    for j in range(i+1,p):
+                        M.constraint(P[j][i,i] == 0)
+            
         
-    
-    if ineq["rlt assignment consistency"]:
-        for i in range(n):
-            for k in range(n):
-                M.constraint(sum([P[j][i,i] + P[j][k,k] - 2 * P[j][i,k] for j in range(p)]) >= 0)
-    
+        if ineq["rlt assignment consistency"]:
+            for i in range(n):
+                for k in range(n):
+                    M.constraint(sum([P[j][i,i] + P[j][k,k] - 2 * P[j][i,k] for j in range(p)]) >= 0)
+
     M.solve()
+    M.acceptedSolutionStatus(AccSolutionStatus.Anything)
 
     currentP = [np.reshape(P[j].level(), (n,n)) for j in range(p)]
     currentB = [np.reshape(B[j].level(), (n,n)) for j in range(p)]
     currentC = [np.reshape(C[j].level(), (n,n)) for j in range(p)]
     currentD = [np.reshape(D[j].level(), (n,n)) for j in range(p)]
     currentZ = [np.reshape(Z[j].level(), (3*n,3*n)) for j in range(p)]
-    currenty = y.level()
+    currenty = np.reshape(y.level(), (p,dim))
 
-    score = 0
+    solution = {
+        "P" : currentP,
+        "B" : currentB,
+        "C" : currentC,
+        "D" : currentD,
+        "y" : currenty
+    }
 
-    for j in range(p):
-        print(currentP[j])
-        eigvals = np.linalg.eigh(currentP[j]).eigenvalues
-        score += eigvals[-1]/p*100
+    info = {
+        "obj" : M.primalObjValue(),
+        "time" : M.getSolverDoubleInfo("optimizerTime"),
+        "constraints" : M.getSolverIntInfo("optNumcon"),
+        "optimalStatus" : M.getPrimalSolutionStatus() == SolutionStatus.Optimal
+    }
 
-        print(eigvals)
-        #print(currentB[j])
-        #print(currentC[j])
-        print()
-    print(currenty)
-    
-    print()
+    return solution, info
 
-    print(f"SCORE = {score}/100") # 100 = EXACTO
-    print()
-
-
-demands = [[0,0],[1,0],[0,1]]
-p = 2
+"""
+demands = [[0,0],[1,0]]
+p = 1
 
 inequalities = {
     "I<P sdp bound" : False,
@@ -172,10 +175,12 @@ inequalities = {
     "distance projection linking cuts" : False,
     "geometric conflict cuts" : False,
     "distance square diameter bounds" : False,
-    "cluster symmetry break" : False
+    "cluster symmetry break" : True
 }
 
-for k in inequalities.keys():
-    inequalities[k] = True
+#for k in inequalities.keys():
+#    inequalities[k] = False
 
-weber(demands, p, inequalities)
+sol, info = weber(demands, p, inequalities)
+print(sol)
+"""
